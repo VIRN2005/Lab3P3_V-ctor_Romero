7558b7042d73dd2667c3a7e2b055fb303672ea26
Víctor Romero <virn2005@gmail.com>
2023-05-05 14:16:08 -0600

    Valores de XOR ingresados

--- Lab3P3_Víctor_Romero.cpp
+++ Lab3P3_Víctor_Romero.cpp
@@ -2,20 +2,119 @@
 using namespace std;
 
 // Ejercicio 1
-int llenarArray() {
-	cout << "-- Menu de Arrays --\n"
-		<< "1) \n"
-		<< "2) \n"
-		<< "3) \n"
-		<< "4) SALIR\n\n";
-	
-	int opcionArray;
-	cout << "Ingrese el Disco a Escoger: ";
-	cin >> opcionArray;
+void llenar_Array() {
+	int disco_1[8];
+	int disco_2[8];
 
-	switch (opcionArray) {
-		
-	}
+	int opcionArray;
+	do {
+		cout << "-- Menu de Arrays --\n"
+			<< "1) Guardar Datos en XOR\n"
+			<< "2) Obtener Datos del Disco 1\n"
+			<< "3) Obtener Datos del Disco 2\n"
+			<< "4) SALIR\n\n";
+
+		cout << "> Ingrese la Opcion a Escoger: ";
+		cin >> opcionArray;
+
+		switch (opcionArray) {
+		case 1: { // Guardar Datos en XOR
+			cout << "Ingrese datos de DISCO 1:" << endl;
+			for (int i = 0; i < 8; i++) {
+				bool es_valido = false;
+				while (!es_valido) {
+					cout << "DISCO 1 [" << i << "]: ";
+					cin >> disco_1[i];
+					if (disco_1[i] == 0 || disco_1[i] == 1) {
+						es_valido = true;
+					}
+					else {
+						cout << "Ingrese solo 1 o 0. Intente de nuevo: ";
+					}
+				}
+			}
+
+			cout << "Los numeros ingresados del DISCO 1 son: [ ";
+			for (int i = 0; i < 8; i++) {
+				cout << disco_1[i] << " ";
+			}
+			cout << "] " << endl << endl;
+
+			cout << "Ingrese datos de DISCO 2:" << endl;
+			for (int i = 0; i < 8; i++) {
+				bool es_valido = false;
+				while (!es_valido) {
+					cout << "DISCO 2 [" << i << "]: ";
+					cin >> disco_2[i];
+					if (disco_2[i] == 0 || disco_2[i] == 1) {
+						es_valido = true;
+					}
+					else {
+						cout << "Ingrese solo 1 o 0. Intente de nuevo: ";
+					}
+				}
+			}
+
+			cout << "Los numeros ingresados del DISCO 2 son: [";
+			for (int i = 0; i < 8; i++) {
+				cout << disco_2[i] << " ";
+			}
+			cout << "] " << endl << endl;
+		}
+			  break;
+
+		case 2: { // Datos del Disco 1
+			cout << "Los numeros ingresados del DISCO 1 son: [ ";
+			for (int i = 0; i < 8; i++) {
+				cout << disco_1[i] << " ";
+			}
+			cout << "] " << endl << endl;
+		}
+			  break;
+
+		case 3: { // Datos del Disco 2
+			cout << "Los numeros ingresados del DISCO 2 son: [";
+			for (int i = 0; i < 8; i++) {
+				cout << disco_2[i] << " ";
+			}
+			cout << "] " << endl << endl;
+		}
+			  break;
+
+		default: {
+			cout << "Valor digitado no es valido\n";
+		}
+		}
+	} while (opcionArray != 4);
+}
+
+void Opcion_1() {
+	int opcionArray;
+	do {
+		cout << "-- Menu de Arrays --\n"
+			<< "1) Guardar Datos en XOR\n"
+			<< "2) Obtener Datos del Disco 1\n"
+			<< "3) Obtener Datos del Disco 2\n"
+			<< "4) SALIR\n\n";
+
+		cout << "Ingrese el Disco a Escoger: ";
+		cin >> opcionArray;
+
+		switch (opcionArray) {
+
+		case 1: {
+			llenar_Array();
+		}
+			  break;
+
+		case 2: {
+
+		}
+			  break;
+
+		}
+
+	} while (opcionArray != 4);
 }
 
 // Menu de Opciones
@@ -25,9 +124,8 @@ int menu() {
 
 	do {
 		cout << "------- MENU ------- \n"
-			<< "1)  \n"
-			<< "2)  \n\n"
-
+			<< "1)  Disco XOR\n"
+			<< "2)  Matriz con Obstaculos\n\n"
 			<< "0) SALIDA \n\n";
 
 		cout << ">> Ingrese una opcion del menu: ";
@@ -43,8 +141,8 @@ int menu() {
 			  break;
 
 		case 1: {
-			cout << "- 1)  - \n";
-
+			cout << "- 1)  Disco XOR- \n";
+			Opcion_1();
 		}
 			  break;
 
